0ae9b01002e285a7ba400376f796b4e112e67ef5
Bogdan Arvinte <bogdan.arvinte@outlook.com>
2021-07-20 19:26:04 +0300

    chore: add icons and sw

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   css: {
@@ -6,4 +7,32 @@ export default defineConfig({
       localsConvention: "camelCaseOnly",
     },
   },
+  plugins: [
+    VitePWA({
+      strategies: "generateSW",
+      registerType: "autoUpdate",
+      includeAssets: ["icons/*.ico"],
+      manifest: {
+        name: "WebAuth ❤️ WebRTC",
+        short_name: "wA❤️wRTC",
+        start_url: "index.html",
+        display: "standalone",
+        background_color: "#000",
+        theme_color: "#37f",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/icons/android-chrome-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/icons/android-chrome-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
 });
